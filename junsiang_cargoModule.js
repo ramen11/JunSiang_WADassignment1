645c29cf72cdbd87fd81a4513ca6e25b9c536a21
ramen11 <kikjunsiang@gmail.com>
2025-11-17 19:04:11 +0800

    some minor change

---
+++ junsiang_cargoModule.js
@@ -0,0 +1,23 @@
+module.exports = {
+    description: "Cargo Modulator",
+    //
+    functionA(a) {
+        return a;
+    },
+    //
+    functionB(a) {
+        return a;
+    },
+    //
+    functionC(c) {
+        return c;
+    },
+        //
+    functionD(c) {
+        return c;
+    },
+        //
+    functionE(c) {
+        return c;
+    }
+}
