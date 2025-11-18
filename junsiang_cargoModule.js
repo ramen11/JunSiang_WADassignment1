e210d1723c8891df04e6edbde9f13bf5357b92f1
ramen11 <kikjunsiang@gmail.com>
2025-11-18 19:22:45 +0800

    Update junsiang_cargoModule.js

--- junsiang_cargoModule.js
+++ junsiang_cargoModule.js
@@ -83,3 +83,70 @@ console.log("This is " + module.exports.description);
 module.exports.maxWeightCapacity(cargo);
 //second function
 module.exports.deliveryCost(cargo, deliveryType);
+
+    //
+    shippingCost(weight, destination) {
+        weight = module.exports.maxWeightCapacity();
+
+        if (weight > 10000){
+            cost = 1500;
+        }else if(weight <= 10000 && weight >1000){
+            cost = 200;
+        }
+        else if(weight <= 10000 && weight <=1000 && weight > 100){
+            cost = 100;
+        }
+        else if(weight <= 10000 && weight <=1000 && weight <= 100 && weight <=10){
+            cost = 1;
+        }
+
+   rates = {
+    1: { name: "India", rate: 1.5, multiplier: 1.5 },
+    2: { name: "Cambodia", rate: 1.49, multiplier: 1.7 },
+    3: { name: "Sri Lanka", rate: 1.44, multiplier: 1.6 },
+    4: { name: "Syria", rate: 1.41, multiplier: 1.2 },
+    5: { name: "Malaysia", rate: 1.24, multiplier: 1.1 },
+    6: { name: "Singapore", rate: 1.10, multiplier: 1.0 }
+    };
+
+
+
+        totalCost = 0;
+        if (b == 1) {
+            totalCost = cost + (country.India * rates.India);
+            console.log("This cargo is going to India");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        else if (b == 2) {
+            totalCost = cost + (country.Cambodia * rates.Cambodia);
+            console.log("This cargo is going to Cambodia");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        else if (b == 3) {
+            totalCost = cost + (country.Sri_Lanka * rates.Sri_Lanka);
+            console.log("This cargo is going to Sri Lanka");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        else if (b == 4) {
+            totalCost = cost + (country.Syria * rates.Syria);
+            console.log("This cargo is going to Syria");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        else if (b == 5) {
+            totalCost = cost + (country.Malaysia * rates.Malaysia);
+            console.log("This cargo is going to Malaysia");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        else if (b == 6) {
+            totalCost = cost + (country.Singapore * rates.Singapore);
+            console.log("This cargo is going to Singapore");
+            console.log("Total shipping cost in SGD is $" + totalCost)
+        }
+        return b ;
+    }
+
+
+console.log("This is " + module.exports.description);
+//fist function 
+module.exports.maxWeightCapacity(100000);
+module.exports.shippingCost(1);
