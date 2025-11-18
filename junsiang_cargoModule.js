0389a00b181e78bc8a98958bc43397a4030e3ca3
ramen11 <kikjunsiang@gmail.com>
2025-11-18 10:18:31 +0800

    update

--- junsiang_cargoModule.js
+++ junsiang_cargoModule.js
@@ -1,6 +1,6 @@
 module.exports = {
     description: "The Cargo Modulation Report",
-    
+
     //
     maxWeightCapacity(a) {
         b = 10000;
@@ -8,7 +8,7 @@ module.exports = {
         d = 250;
         e = 5;
 
-        console.log("This cargo is " + a +"kg");
+        console.log("This cargo is " + a + "kg");
 
         if (a > b) {
             console.log("Deny on ship!!! Overweight by: " + (a - b) + "kg");
@@ -32,7 +32,7 @@ module.exports = {
             console.log("Acceptable on ship");
             console.log("Acceptable on plane");
             console.log("Acceptable on Truck");
-             console.log("Deny on hand carry!!! Overweight by: " + (a-e) + "kg");
+            console.log("Deny on hand carry!!! Overweight by: " + (a - e) + "kg");
         }
         else if (a <= b && a <= c && a <= d && a <= e) {
             console.log("Acceptable on ship");
@@ -42,8 +42,56 @@ module.exports = {
         }
         return a;
     },
-}
 
-console.log ("This is " + module.exports.description);
+
+    // deliverySpeedCost(a, option) {
+    //     const deliveryFee = {
+    //         1: 10,  // Enterprise
+    //         2: 40,  // Advanced
+    //         3: 30   // Pro (corrected from 50 to match your variable)
+    //     };
+
+    //     const weight = this.maxWeightCapacity(a);
+    //     let cost = weight >= 1000 ? 100 : 50;
+
+    //     if (![1, 2, 3].includes(option)) {
+    //         console.log("Please choose 1 to 3 only!");
+    //         return null;
+    //     }
+
+    //     return cost + deliveryFee[option];
+    // }
+
+    //sort out 
+    deliveryCost(a, option){
+
+        if (this.maxWeightCapacity(a) >= 1000 ){
+             cost = 100;
+        }
+        else if (this.maxWeightCapacity(a) <= 1000 ){
+             cost = 50;
+        }
+
+        deliveryFee = {
+            entry:10,
+            pro:40,
+            max:50
+        };
+
+        if (!["entry", "pro", "max"].includes(option)) {
+            console.log("Please choose entry, pro, or max only!");
+            return null;
+        }
+        else{
+            console.log("The total delivery cost is " + module.exports.deliveryCost(this.maxWeightCapacity(a), deliveryFee[option]));
+        }
+
+        return cost  + deliveryFee[option];
+    },
+}
+const cargo = 500;
+const deliveryType = "entry";
+console.log("This is " + module.exports.description);
 //fist function 
-module.exports.maxWeightCapacity(100000);
+module.exports.maxWeightCapacity(cargo);
+module.exports.deliveryCost(cargo, deliveryType);
